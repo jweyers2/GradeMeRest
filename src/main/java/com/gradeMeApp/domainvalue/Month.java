c826d18b404d96e50a0e57156a5a38eb1cff4800
Okamihokori <44642203+Okamihokori@users.noreply.github.com>
2019-06-16 18:59:58 +0200

    add more month

--- src/main/java/com/gradeMeApp/domainvalue/Month.java
+++ src/main/java/com/gradeMeApp/domainvalue/Month.java
@@ -1,8 +1,6 @@
 package com.gradeMeApp.domainvalue;
 
 public enum Month {
- 
-	JANUARY,
-	FEBRUARY,
-	MARCH
+
+	JANUARY, FEBRUARY, MARCH, APRIL, MAY, JUNE, JULY, AUGUST, SEPTEMBER, OCTOBER, NOVEMBER, DECEMBER
 }
